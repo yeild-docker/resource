32702961bc872a3422463b398c34e9043b997709
935057137@qq.com <935057137@qq.com>
2019-10-29 22:04:09 +0800

    init

--- centos-7-init.sh
+++ centos-7-init.sh
@@ -4,18 +4,31 @@
 
 yum update -y
 yum install -y openssh-server vim wget gcc gcc-c++ automake autoconf libtool make zlib zlib-devel openssl openssl-devel lsof unzip zip bzip2 net-tools passwd cracklib-dicts intltool kde-l10n-Chinese pcre pcre-devel expect
+
 # sshd
 echo root:eshxcmhk | chpasswd
-ssh_host_rsa_key
-spawn ssh-keygen -t rsa -f /etc/ssh/ssh_test
-expect {
- "Enter passphrase" { send "\r"; exp_continue }
- "Enter same passphrase again" { send "\r"; exp_continue }
- eof {;}
-}
+sed -i 's/#Port 22/Port 22/g' /etc/ssh/sshd_config
+sed -i 's/PermitRootLogin no/PermitRootLogin yes/g' /etc/ssh/sshd_config
+sed -i 's/#PermitRootLogin yes/PermitRootLogin yes/g' /etc/ssh/sshd_config
+expect<<!
+spawn ssh-keygen -t rsa -f /etc/ssh/ssh_host_rsa_key
+expect "Enter passphrase" { send "\r" }
+expect "Enter same passphrase again" { send "\r" }
 expect eof
-ssh-keygen -t rsa -f /etc/ssh/ssh_host_ecdsa_key
-ssh-keygen -t rsa -f /etc/ssh/ssh_host_ed25519_key
+!
+expect<<!
+spawn ssh-keygen -t rsa -f /etc/ssh/ssh_host_ecdsa_key
+expect "Enter passphrase" { send "\r" }
+expect "Enter same passphrase again" { send "\r" }
+expect eof
+!
+expect<<!
+spawn ssh-keygen -t rsa -f /etc/ssh/ssh_host_ed25519_key
+expect "Enter passphrase" { send "\r" }
+expect "Enter same passphrase again" { send "\r" }
+expect eof
+!
+
 systemctl enable sshd && systemctl start sshd
 
 # timezone
