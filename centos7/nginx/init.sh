c0b00095258efcc72e105cfdf5243f4588e68129
935057137@qq.com <935057137@qq.com>
2019-10-30 14:10:37 +0800

    Update init.sh

--- centos7/nginx/init.sh
+++ centos7/nginx/init.sh
@@ -1,5 +1,5 @@
 
-yum install -y wget gcc gcc-c++ automake autoconf make
+yum install -y wget gcc gcc-c++ automake autoconf make pcre pcre-devel
 cmd_rs=$?; if [ $cmd_rs -ne 0 ]; then exit $cmd_rs; fi
 
 wget https://nginx.org/download/nginx-1.16.1.tar.gz && tar -zxvf nginx-1.16.1.tar.gz && cd nginx-1.16.1 && ./configure --prefix=/usr/local/nginx && make && make install && echo "export PATH=/usr/local/nginx/sbin:\$PATH" >> /etc/profile && source /etc/profile && cd .. && rm -rf nginx-1.16.1* && wget -P /lib/systemd/system https://raw.githubusercontent.com/yeild-docker/resource/master/centos7/nginx/nginx.service && systemctl enable nginx
