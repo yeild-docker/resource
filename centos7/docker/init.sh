7ef4cbca88e4f6776531061b41ae0444e074b575
935057137@qq.com <935057137@qq.com>
2019-11-04 13:33:16 +0800

    Update init.sh

--- centos7/docker/init.sh
+++ centos7/docker/init.sh
@@ -1,6 +1,7 @@
 
 # _VERSION=-19.03.4
 _VERSION=""
+_CENTOS=`sed -n "s|^.*CentOS[[:blank:]]*release[[:blank:]]*\([0-9]*\)\..*$|\1|p" /etc/redhat-release`
 
 while getopts ":hv:" opt
 do
@@ -18,21 +19,35 @@ do
 	esac
 done
 
-yum install -y yum-utils device-mapper-persistent-data lvm2
-cmd_rs=$?; if [ $cmd_rs -ne 0 ]; then exit $cmd_rs; fi
-yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
-cmd_rs=$?; if [ $cmd_rs -ne 0 ]; then exit $cmd_rs; fi
-echo "To install a specific version of Docker Engine - Community, list the available versions in the repo, then select and install"
-echo "List and sort the versions available in your repo. This example sorts results by version number, highest to lowest, and is truncated:"
-echo "yum list docker-ce --showduplicates | sort -r"
-yum install -y docker-ce$_VERSION docker-ce-cli$_VERSION containerd.io
+case $_CENTOS in
+	6)
+	rpm -iUvh http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
+	cmd_rs=$?; if [ $cmd_rs -ne 0 ]; then echo "Exit with Fail!"; exit $cmd_rs; fi
+	yum update -y
+	cmd_rs=$?; if [ $cmd_rs -ne 0 ]; then echo "Exit with Fail!"; exit $cmd_rs; fi
+	yum -y install docker-io
+	cmd_rs=$?; if [ $cmd_rs -ne 0 ]; then echo "Exit with Fail!"; exit $cmd_rs; fi
+	chkconfig docker on
+	service docker start
+	;;
+	7)
+	yum install -y yum-utils device-mapper-persistent-data lvm2
+	cmd_rs=$?; if [ $cmd_rs -ne 0 ]; then echo "Exit with Fail!"; exit $cmd_rs; fi
+	yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+	cmd_rs=$?; if [ $cmd_rs -ne 0 ]; then echo "Exit with Fail!"; exit $cmd_rs; fi
+	echo "To install a specific version of Docker Engine - Community, list the available versions in the repo, then select and install"
+	echo "List and sort the versions available in your repo. This example sorts results by version number, highest to lowest, and is truncated:"
+	echo "yum list docker-ce --showduplicates | sort -r"
+	yum install -y docker-ce$_VERSION docker-ce-cli$_VERSION containerd.io
+	cmd_rs=$?; if [ $cmd_rs -ne 0 ]; then echo "Exit with Fail!"; exit $cmd_rs; fi
+	systemctl enable docker
+	systemctl start docker
+	;;
+esac
 cmd_rs=$?; if [ $cmd_rs -ne 0 ]; then exit $cmd_rs; fi
 if ! [[ `sysctl net.ipv4.ip_forward` =~ net\.ipv4\.ip_forward[[:blank:]]=[[:blank:]]1 ]]; then
 	echo -e "\nnet.ipv4.ip_forward=1\n" >> /etc/sysctl.conf && systemctl restart network
 fi
-
-systemctl enable docker
-systemctl start docker
 
 exit 0
 
