13a4535f6f9bf0ab4742a03a1621de7ec2b45a57
935057137@qq.com <935057137@qq.com>
2019-12-19 16:38:27 +0800

    Update init.sh

--- centos7/python3/init.sh
+++ centos7/python3/init.sh
@@ -1,8 +1,14 @@
 path=/usr/local/python3
-openssl=--with-openssl=/usr/local/openssl
+openssl=/usr/local/openssl
+with-openssl=--with-openssl=$openssl
 
+if [ ! -d $openssl ]; then
+	echo "Install openssl to ${openssl}"
+	curl -fsSL "https://raw.githubusercontent.com/yeild-docker/resource/master/centos7/openssl/init.sh" | sh
+	cmd_rs=$?; if [ $cmd_rs -ne 0 ]; then exit $cmd_rs; fi
+fi
 yum install -y wget gcc make zlib zlib-devel
-wget https://www.python.org/ftp/python/3.7.5/Python-3.7.5.tgz -O Python-3.7.5.tgz && tar -zxvf Python-3.7.5.tgz && cd Python-3.7.5 && ./configure -prefix=$path $openssl && make && make install && cd .. && rm -rf Python-3.7.5* && ln -s $path/bin/python3 /usr/bin/python3 && ln -s $path/bin/pip3 /usr/bin/pip3
+wget https://www.python.org/ftp/python/3.7.5/Python-3.7.5.tgz -O Python-3.7.5.tgz && tar -zxvf Python-3.7.5.tgz && cd Python-3.7.5 && ./configure -prefix=$path $with-openssl && make && make install && cd .. && rm -rf Python-3.7.5* && ln -s $path/bin/python3 /usr/bin/python3 && ln -s $path/bin/pip3 /usr/bin/pip3
 cmd_rs=$?; if [ $cmd_rs -ne 0 ]; then exit $cmd_rs; fi
 
 exit 0
