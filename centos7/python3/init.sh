6a55013368084144833e2d64c693fd524d899c05
935057137@qq.com <935057137@qq.com>
2019-12-19 16:06:36 +0800

    openssl

--- centos7/python3/init.sh
+++ centos7/python3/init.sh
@@ -1,6 +1,6 @@
 path=/usr/local/python3
 
-yum install -y wget gcc make zlib zlib-devel
+yum install -y wget gcc make zlib zlib-devel openssl-devel
 wget https://www.python.org/ftp/python/3.7.5/Python-3.7.5.tgz -O Python-3.7.5.tgz && tar -zxvf Python-3.7.5.tgz && cd Python-3.7.5 && ./configure -prefix=$path && make && make install && cd .. && rm -rf Python-3.7.5* && ln -s $path/bin/python3 /usr/bin/python3 && ln -s $path/bin/pip3 /usr/bin/pip3
 cmd_rs=$?; if [ $cmd_rs -ne 0 ]; then exit $cmd_rs; fi
 
