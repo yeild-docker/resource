9c51ddfbdaa3c3e2e6389ec62073cb4f84dbdbff
935057137@qq.com <935057137@qq.com>
2019-12-20 13:38:10 +0800

    Update init.sh

--- centos7/python3/init.sh
+++ centos7/python3/init.sh
@@ -10,5 +10,7 @@ fi
 yum install -y wget gcc make zlib zlib-devel
 wget -c https://www.python.org/ftp/python/3.7.5/Python-3.7.5.tgz -O Python-3.7.5.tgz && tar -zxvf Python-3.7.5.tgz && cd Python-3.7.5 && ./configure --prefix=${path} ${with_openssl} && make && make install && cd .. && rm -rf Python-3.7.5* && ln -s ${path}/bin/python3 /usr/bin/python3 && ln -s ${path}/bin/pip3 /usr/bin/pip3
 cmd_rs=$?; if [ $cmd_rs -ne 0 ]; then exit $cmd_rs; fi
+echo "export PATH=\$PATH:${path}/bin" >> /etc/profile
+source /etc/profile
 
 exit 0
