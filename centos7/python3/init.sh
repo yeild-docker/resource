59e6200d715ec9ccd657957aabae11484ed1714b
935057137@qq.com <935057137@qq.com>
2019-12-20 13:06:18 +0800

    up

--- centos7/python3/init.sh
+++ centos7/python3/init.sh
@@ -1,6 +1,6 @@
-path='/usr/local/python3'
-openssl='/usr/local/openssl'
-with-openssl='--with-openssl=$openssl'
+path="/usr/local/python3"
+openssl="/usr/local/openssl"
+with_openssl="--with-openssl=${openssl}"
 
 if [ ! -d $openssl ]; then
 	echo "Install openssl to ${openssl}"
@@ -8,7 +8,7 @@ if [ ! -d $openssl ]; then
 	cmd_rs=$?; if [ $cmd_rs -ne 0 ]; then exit $cmd_rs; fi
 fi
 yum install -y wget gcc make zlib zlib-devel
-wget https://www.python.org/ftp/python/3.7.5/Python-3.7.5.tgz -O Python-3.7.5.tgz && tar -zxvf Python-3.7.5.tgz && cd Python-3.7.5 && ./configure -prefix=$path $with-openssl && make && make install && cd .. && rm -rf Python-3.7.5* && ln -s $path/bin/python3 /usr/bin/python3 && ln -s $path/bin/pip3 /usr/bin/pip3
+wget -c https://www.python.org/ftp/python/3.7.5/Python-3.7.5.tgz -O Python-3.7.5.tgz && tar -zxvf Python-3.7.5.tgz && cd Python-3.7.5 && ./configure --prefix=${path} ${with_openssl} && make && make install && cd .. && rm -rf Python-3.7.5* && ln -s ${path}/bin/python3 /usr/bin/python3 && ln -s ${path}/bin/pip3 /usr/bin/pip3
 cmd_rs=$?; if [ $cmd_rs -ne 0 ]; then exit $cmd_rs; fi
 
 exit 0
