55f2a7bf8ed207fdf2adbe89cc67250665ab57ab
935057137@qq.com <935057137@qq.com>
2020-04-02 11:27:39 +0800

    u

--- centos7/sqlite3/init.sh
+++ centos7/sqlite3/init.sh
@@ -1,6 +1,13 @@
 path="/usr/local/sqlite3"
-yum install -y wget gcc gcc-c++ make zlib zlib-devel libffi libffi-devel openssl openssl-devel
+openssl="/usr/local/openssl"
+
+yum install -y wget gcc gcc-c++ make zlib zlib-devel libffi libffi-devel
 sqlite3_ver=3310100
+if [ ! -d $openssl ]; then
+	echo "Install openssl to ${openssl}"
+	curl -fsSL "https://raw.githubusercontent.com/yeild-docker/resource/master/centos7/openssl/init.sh" | sh
+	cmd_rs=$?; if [ $cmd_rs -ne 0 ]; then exit $cmd_rs; fi
+fi
 wget -c https://www.sqlite.org/2020/sqlite-autoconf-${sqlite3_ver}.tar.gz -O sqlite-autoconf-${sqlite3_ver}.tar.gz && tar -zxvf sqlite-autoconf-${sqlite3_ver}.tar.gz && cd sqlite-autoconf-${sqlite3_ver} && CFLAGS="-Os -DSQLITE_THREADSAFE=2" ./configure --prefix=${path}&& make -j8 && make install && cd .. && rm -rf sqlite-autoconf-${sqlite3_ver}*
 cmd_rs=$?; if [ $cmd_rs -ne 0 ]; then exit $cmd_rs; fi
 mv /usr/bin/sqlite3  /usr/bin/sqlite3_old
