6df9529acc0979de3f67ae0e88170bc3c5173881
935057137@qq.com <935057137@qq.com>
2020-04-02 15:25:58 +0800

    Update init.sh

--- centos7/python3/init.sh
+++ centos7/python3/init.sh
@@ -1,5 +1,6 @@
 path="/usr/local/python3"
 openssl="/usr/local/openssl"
+sqlite3="/usr/local/sqlite3"
 with_openssl="--with-openssl=${openssl}"
 
 if [ ! -d $openssl ]; then
@@ -7,13 +8,18 @@ if [ ! -d $openssl ]; then
 	curl -fsSL "https://raw.githubusercontent.com/yeild-docker/resource/master/centos7/openssl/init.sh" | sh
 	cmd_rs=$?; if [ $cmd_rs -ne 0 ]; then exit $cmd_rs; fi
 fi
+if [ ! -d $sqlite3 ]; then
+	echo "Install sqlite3 to ${sqlite3}"
+	curl -fsSL "https://raw.githubusercontent.com/yeild-docker/resource/master/centos7/sqlite3/init.sh" | sh
+	cmd_rs=$?; if [ $cmd_rs -ne 0 ]; then exit $cmd_rs; fi
+fi
 yum install -y wget gcc gcc-c++ make zlib zlib-devel libffi libffi-devel
 wget -c https://www.python.org/ftp/python/3.7.6/Python-3.7.6.tgz -O Python-3.7.6.tgz
 cmd_rs=$?; if [ $cmd_rs -ne 0 ]; then exit $cmd_rs; fi
 tar -zxvf Python-3.7.6.tgz && cd Python-3.7.6
-./configure --enable-shared --enable-optimizations --enable-loadable-sqlite-extensions --with-system-expat --with-system-ffi --with-ensurepip=yes --prefix=${path} ${with_openssl}
+./configure LDFLAGS="-L${sqlite3}/lib" CPPFLAGS="-I${sqlite3}/include" --enable-shared --enable-optimizations --enable-loadable-sqlite-extensions --with-system-expat --with-system-ffi --with-ensurepip=yes --prefix=${path} ${with_openssl}
 cmd_rs=$?; if [ $cmd_rs -ne 0 ]; then exit $cmd_rs; fi
-processor=`expr \`grep processor /proc/cpuinfo 2>&1 | wc -l\` \* 3 / 4 + 1`
+processor=`expr \`grep processor /proc/cpuinfo 2>&1 | wc -l\` \* 2 / 3 + 1`
 make -j $processor && make install
 cmd_rs=$?; if [ $cmd_rs -ne 0 ]; then exit $cmd_rs; fi
 cd .. && rm -rf Python-3.7.6*
