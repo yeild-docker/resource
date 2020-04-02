d92871fee170285eaf04600fc2037e019c315110
935057137@qq.com <935057137@qq.com>
2020-04-02 11:39:34 +0800

    Update init.sh

--- centos7/python3/init.sh
+++ centos7/python3/init.sh
@@ -7,7 +7,7 @@ if [ ! -d $openssl ]; then
 	curl -fsSL "https://raw.githubusercontent.com/yeild-docker/resource/master/centos7/openssl/init.sh" | sh
 	cmd_rs=$?; if [ $cmd_rs -ne 0 ]; then exit $cmd_rs; fi
 fi
-yum install -y wget gcc gcc-c++ make zlib zlib-devel libffi libffi-devel openssl openssl-devel
+yum install -y wget gcc gcc-c++ make zlib zlib-devel libffi libffi-devel
 wget -c https://www.python.org/ftp/python/3.7.6/Python-3.7.6.tgz -O Python-3.7.6.tgz && tar -zxvf Python-3.7.6.tgz && cd Python-3.7.6 && ./configure --enable-shared --enable-optimizations --prefix=${path} ${with_openssl} && make -j8 && make install && cd .. && rm -rf Python-3.7.6*
 cmd_rs=$?; if [ $cmd_rs -ne 0 ]; then exit $cmd_rs; fi
 ln -s ${path}/bin/python3 /usr/bin/python3 && ln -s ${path}/bin/pip3 /usr/bin/pip3
